6c05918805d61a3594c59e14de6e6867722b6564
TatyanaTikhonova <tatyana.tikhonova.99@mail.ru>
2019-09-26 12:23:09 +0400

    Added entry point

---
+++ src/PO72/Tikhonova/wdad/Applicatoin.java
@@ -0,0 +1,8 @@
+package PO72.Tikhonova.wdad;
+
+public class Applicatoin {
+    public static void main(String[] args)
+    {
+        System.out.println("I’m Tanya Tikhonova, and I’m not a monkey");
+    }
+}
